87bab935d6640b1f8868acbf74f83d0cf44393bc
MaxfeedLeyn <maxnebelskiy@gmail.com>
2026-02-01 22:35:14 +0200

    Remove unused values

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -27,7 +27,7 @@ export async function fetchNotes(query: string, page: number) {
 }
 
 export async function createNote(note: PostNote) {
-  const response = await axios.post(
+  await axios.post(
     'https://notehub-public.goit.study/api/notes', note, 
     {
       headers: {
@@ -38,7 +38,7 @@ export async function createNote(note: PostNote) {
 }
 
 export async function deleteNote(id: string) {
-  const response = await axios.delete<Note>(
+  await axios.delete<Note>(
     `https://notehub-public.goit.study/api/notes/${id}`,
     {
       headers: {
